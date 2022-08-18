7a01badeae13ce812c10fffc18257a94375eac5d
TheCountt <isaacadedeji0@yahoo.com>
2022-08-18 15:54:27 +0000

    commit

--- backend.tf
+++ backend.tf
@@ -1,7 +1,21 @@
 # # Introducing Backend on S3 
 resource "aws_s3_bucket" "k8s-terraform_state" {
   bucket = "k8s-terraform-bucket"
+  acl    = "log-delivery-write"
+  
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "AES256"
+      }
+    }
+  }
 }
+
 
 resource "aws_s3_bucket_public_access_block" "example-public-access-block" {
   bucket = aws_s3_bucket.example.id
@@ -12,23 +26,6 @@ resource "aws_s3_bucket_public_access_block" "example-public-access-block" {
   restrict_public_buckets = true
 }
 
-  # Enable versioning so we can see the full revision history of our state files
-resource "aws_s3_bucket_versioning" "k8s_terraform_bucket_versioning" {
-  bucket = aws_s3_bucket.k8s-terraform_state.bucket
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-  # Enable server-side encryption by default
-resource "aws_s3_bucket_server_side_encryption_configuration" "k8s_terraform_bucket encrypytion" {
-  bucket = aws_s3_bucket.k8s-terraform_state.bucket
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
 
 resource "aws_dynamodb_table" "k8s-terraform_locks" {
   name         = "k8s-terraform-bucket-locks"
