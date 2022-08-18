4f2383f2f6d0ae0cd6fe9e305174073d9c84a408
TheCountt <isaacadedeji0@yahoo.com>
2022-08-18 15:30:30 +0000

    fixing security hotspots

--- backend.tf
+++ backend.tf
@@ -1,16 +1,31 @@
 # # Introducing Backend on S3 
 resource "aws_s3_bucket" "k8s-terraform_state" {
   bucket = "k8s-terraform-bucket"
+}
+
+resource "aws_s3_bucket_public_access_block" "example-public-access-block" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
   # Enable versioning so we can see the full revision history of our state files
-  versioning {
-    enabled = true
+resource "aws_s3_bucket_versioning" "k8s_terraform_bucket_versioning" {
+  bucket = aws_s3_bucket.k8s-terraform_state.bucket
+  versioning_configuration {
+    status = "Enabled"
   }
+}
   # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+resource "aws_s3_bucket_server_side_encryption_configuration" "k8s_terraform_bucket encrypytion" {
+  bucket = aws_s3_bucket.k8s-terraform_state.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
 }
