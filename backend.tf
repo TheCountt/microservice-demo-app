d7630c948a8f5ed8305ce03cdc192c5ce74e09a9
TheCountt <isaacadedeji0@yahoo.com>
2022-10-26 16:49:34 +0000

    commit

--- backend.tf
+++ backend.tf
@@ -1,31 +1,36 @@
 # # Introducing Backend on S3 
-resource "aws_s3_bucket" "k8s-terraform_state1" {
-  bucket = "k8s-terraform-bucket2"
-  acl    = "log-delivery-write"
-  
-  versioning {
-    enabled = true
-    # mfa_delete = false
-     }
+resource "aws_s3_bucket" "k8s-terraform_state" {
+  bucket = "k8s-terraform-stateqa"
+# Prevents accidental deletion of the S3 Bucket
+#  lifecycle {
+#    prevent_destroy =true
+#  }
+}
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm     = "AES256"
-      }
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket  = aws_s3_bucket.k8s-terraform_state.id
+  versioning_configuration {
+    status  = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket  = aws_s3_bucket.k8s-terraform_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm   = "AES256"
     }
   }
 }
 
-
-/* resource "aws_s3_bucket_public_access_block" "example-public-access-block" {
-  bucket = "k8s-terraform-bucket"
-
+# Explicity block all public access to the S3 bucket
+resource "aws_s3_bucket_public_access_block" "example-public-access-block" {
+  bucket  = aws_s3_bucket.k8s-terraform_state.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-} */
+}
 
 resource "aws_dynamodb_table" "k8s-terraform_locks" {
   name         = "k8s-terraform-bucket-locks"
@@ -37,12 +42,12 @@ resource "aws_dynamodb_table" "k8s-terraform_locks" {
   }
 }
 
- terraform {
+ /* terraform {
    backend "s3" {
-     bucket         = "k8s-terraform-bucket2"
+     bucket         = "k8s-terraform-stateqa"
      key            = "global/s3/terraform.tfstate"
      region         = "us-west-1"
      dynamodb_table = "k8s-terraform-bucket-locks"
     encrypt        = true
    }
- }
+ } */
--- manifests/README.md
+++ manifests/README.md
@@ -1,11 +1,38 @@
-# Kubernetes manifests
+# Implementing RBAC on K8S cluster
+
+## Pre-requisites
+
+- Created IAM User Groups with IaC with group users included in the K8S infra code (check **aws-terraform/terraform/iam.tf**)
+
+### Steps
+
+- Enable k8s RBAC on the IAM Groups
+
+- Create a Namespace (***Development***, in this case, was created) for developers
+
+- Scope IAM Developer User Group  to ***Development*** namespace so that group
+users can only deploy workloads in the ***Development*** namespace
+
+- Generate a kubeconfig file  so that Group users will be able to connect to the
+clusters and perform operations they have permissions for.
+
+
+# Set Resource Quotas on Namespace(s)
+
+For more information, consult the Kubernetes Documentation.
+
+# Deployment of the Microservice Application
 
 Create the kubernetes objects in this order:
 
 - storageclass.yml
+
 - secret.yml (this is for experimental purposes only, never save secrets in version control)
+
 - mongo-statefulset.yml
+
 - configmap.yml (this is for experimental purposes only, never save configmap in version control)
+
 - mern-app.yml
 
 Statefulset object is best to run stateful applications such as databases,etc.
